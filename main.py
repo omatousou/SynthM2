fcb6c41f1cab0babde3392102a2fca49d8ced43b
omatousou <omatousou@users.noreply.github.com>
2026-02-05 18:20:52 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+
 import sys
 import numpy as np
 from math import gcd
@@ -11,31 +12,64 @@ from interface import SynthInterface
 
 class AppController:
     def __init__(self):
-        """
-        ATTRIBUTS :
-        self.app : QApplication principale
-        self.gui : interface graphique
-        self.audio : moteur audio
-        self.gen : générateur de signal
-        self.NOTES_MAP : dictionnaire touches/fréquences
-        self.is_playing : booléen lecture en cours
-        self.active_freqs : fréquences actives
-        self.previous_freqs : fréquences précédentes
-        self.waiting : booléen attente fin de période
-        self.current_time : temps courant
-        self.phase_accum : accumulateur de phase
-        self.plot_buffer : buffer pour affichage
-        self.timer : QTimer pour lecture par blocs
-        self.wait_timer : QTimer pour attente fin de période
-        self.gen.fs : fréquence d'échantillonnage (via self.gen)
-        self.combo : sélection du type d'onde (via self.gui)
-        self.close_signal : signal de fermeture (via self.gui)
-        """
-        pass
+
+        ## ATTRIBUTS :
+        self.app = QApplication(sys.argv) # Application Qt. sys.argv : argument vector est la liste des paramètres envoyés au programme lors de son lancement. Ici contient le chemin vers le fichier Python.
+                                          # Exemple : python mon_jeu.py --fullscreen --level 5 ALORS sys.argv[0] : "mon_jeu.py" sys.argv[1] : "--fullscreen" sys.argv[2] : "--level"sys.argv[3] : "5"
+        self.gui = SynthInterface() # Import de la classe SynthInterface dans le fichier interface.py
+        self.audio = AudioEngine() # Import de la classe AudioEngine dans le fichier audio_engine.py
+        self.gen = SignalGenerator() # Import de la classe SignalGenerator dans le fichier generator.py
+
+
+        # f = 440 * (2 ** ((n - 69) / 12)):  convention MIDI, où la note 69 correspond au La4 (440 Hz). http://antoinegabrielbrun.com/ressources/frequence-des-notes-de-la-gamme/
+        calc_freq = lambda n: round(440 * (2 ** ((n - 69) / 12)), 2)
+
+        # Touche Midi : Frequences associées
+
+        self.NOTES_MAP = {
+            # Touches Blanches
+            Qt.Key_Q: calc_freq(60),  # Do (C4)
+            Qt.Key_S: calc_freq(62),  # Ré
+            Qt.Key_D: calc_freq(64),  # Mi
+            Qt.Key_F: calc_freq(65),  # Fa
+            Qt.Key_G: calc_freq(67),  # Sol
+            Qt.Key_H: calc_freq(69),  # La
+            Qt.Key_J: calc_freq(71),  # Si
+            Qt.Key_K: calc_freq(72),  # Do (C5)
+            Qt.Key_L: calc_freq(74),  # Ré
+
+            # Touches Noires
+            Qt.Key_Z: calc_freq(61),  # Do#
+            Qt.Key_E: calc_freq(63),  # Ré#
+            Qt.Key_T: calc_freq(66),  # Fa#
+            Qt.Key_Y: calc_freq(68),  # Sol#
+            Qt.Key_U: calc_freq(70),  # La#
+            Qt.Key_O: calc_freq(73),  # Do#
+            Qt.Key_P: calc_freq(75),  # Ré#
+}
+
+
+        self.is_playing = False # Indique si une note est en cours de lecture
+        self.active_freqs = set() # Fréquences actuellement jouées. Utilisation d'un set pour éviter les doublons
+        self.previous_freqs = set() # Fréquences jouées avant l'arrêt. Utilisation d'un set pour éviter les doublons
+        self.waiting = False # Indique si on est en période d'attente avant d'arrêter le son
+        self.current_time = 0.0 # Temps courant en secondes
+        self.phase_accum = {}  # Accumulateur de phase pour chaque fréquence
+        self.plot_buffer = []  # Buffer pour l'affichage graphique
+
+        # Configure une alarme qui déclenchera la fonction "play_block" à chaque fois que le délai sera écoulé.
+        self.timer = QTimer() #in
+        self.timer.timeout.connect(self.play_block) # timeout signale émis lorsque le délai est écoulé (délai défini par start) et appelle play_block. Le lien est fait via la méthode connect.
+
+        # Connexion des signaux de l'interface
+        # Connecte les événements d'entrée (clavier et fermeture) aux fonctions de gestion correspondantes.
+        self.gui.key_pressed.connect(self.on_press) # Si une touche est pressée, appelle on_press. Le lien est fait via la méthode connect.
+        self.gui.key_released.connect(self.on_release) # Si une touche est relâchée, appelle on_release. Le lien est fait via la méthode connect.
+        self.gui.close_signal.connect(self.on_close) # Si la fenêtre est fermée, appelle on_close. Le lien est fait via la méthode connect.
 
     def on_press(self, key):
         """
-        1. Vérifie si la touche pressée est dans self.NOTES_MAP.
+        1. Récupère et vérifie si la touche pressée est dans self.NOTES_MAP.
         2. Si oui, récupère la fréquence associée à la touche.
         3. Ajoute cette fréquence à self.active_freqs.
         4. Si la fréquence n'est pas déjà dans self.phase_accum :
@@ -45,7 +79,20 @@ class AppController:
             a. Passe self.is_playing à True.
             b. Démarre le timer pour jouer les blocs audio toutes les 25 ms.
         """
-        pass
+        key_freq = self.NOTES_MAP.get(key) # Récupère la fréquence associée à la touche pressée
+        if key_freq: # Si la touche est dans self.NOTES_MAP
+            self.active_freqs.add(key_freq) # Ajoute la fréquence à l'ensemble des fréquences actives
+            # Pour eviter les glitches, on synchronise la phase des nouvelles notes avec la moyenne des phases existantes
+            if key_freq not in self.phase_accum: # Si la fréquence n'a pas encore de phase accumulée
+                if self.phase_accum: # S'il y a déjà des phases accumulées
+                    avg_phase = np.mean(list(self.phase_accum.values())) # Calcule la moyenne des phases existantes
+                    self.phase_accum[key_freq] = avg_phase # Initialise la phase de la nouvelle fréquence avec la moyenne
+                else:
+                    self.phase_accum[key_freq] = 0.0 # Sinon, initialise la phase à 0
+
+            if not self.is_playing and self.active_freqs: # Si aucune note n'était en cours de lecture et qu'il y a au moins une fréquence active
+                self.is_playing = True # Passe is_playing à True
+                self.timer.start(25)  # Joue les blocs audio toutes les 25 ms
 
     def on_release(self, key):
         """
@@ -56,14 +103,43 @@ class AppController:
         4. Retire la fréquence de self.active_freqs.
         5. Si plus aucune fréquence n'est active :
             a. Passe self.is_playing à False et self.waiting à True.
-            b. Calcule la période d'attente (LCM des périodes des fréquences précédentes).
+            b. Calcule la période d'attente (PGCD (LCM) des périodes des fréquences précédentes).
             c. Crée un QTimer (self.wait_timer) pour attendre la fin de la période.
         6. Sinon, si d'autres notes restent actives :
             a. Met à jour l'affichage du signal restant sur l'interface.
         """
-        pass
+        key_freq = self.NOTES_MAP.get(key) # Récupère la fréquence associée à la touche pressée
+        if key_freq: # Si la touche est dans self.NOTES_MAP
+            self.previous_freqs = self.active_freqs.copy() # Copie l'ensemble des fréquences actives dans self.previous_freqs
+            self.active_freqs.remove(key_freq) # Retire la fréquence de l'ensemble des fréquences actives
+            if not self.active_freqs: # Si plus aucune fréquence n'est active
+                self.is_playing = False # Passe is_playing à False
+                self.waiting = True # Passe waiting à True
+
+                self.wait_timer = QTimer() # Crée un QTimer pour gérer la période d'attente
+                # Calcul de la période d'attente comme le PPCM des périodes des fréquences précédentes  
+                periods = [Fraction(1, f).limit_denominator(1000) for f in self.previous_freqs] # Liste des périodes sous forme de fractions les plus simples maximum 1/1000
+                wait_time = np.lcm(periods)
+                self.wait_timer = QTimer()
+                self.wait_timer.setSingleShot(True)
+                self.wait_timer.timeout.connect(self.end_wait)
+                self.wait_timer.start(int(wait_time * 1000))
+            elif self.is_playing:
+                # Met à jour l'affichage du signal restant
+                duration = 0.025 # Durée de 25 ms pour l'affichage
+                t = np.linspace(0, duration, int(duration * self.gen.fs), endpoint=False) # Vecteur temps  
+                sig = np.zeros_like(t) # Initialise le signal à zéro
+                for freq in self.active_freqs:
+                    sig += self.gen.get_block(list(self.active_freqs), self.phase_accum, duration, self.gui.get_wave_type()) # Génère le signal audio
+                self.gui.update_display(t, sig, list(self.active_freqs)) # Met à jour l'affichage graphique
+
 
     def play_block(self):
+        if self.is_playing or self.waiting:
+            if self.waiting:
+                freqs = self.previous_freqs
+            else:
+                freqs = self.active_freqs   
         """
 
         1. Vérifie si self.is_playing ou self.waiting est vrai.
